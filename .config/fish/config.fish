9fd038ae22592d23e920721a63882a7e519348e9
Michael Krasnyk <michael.krasnyk@aid-driving.eu>
2020-04-17 06:17:01 +0200

    Added bazelisk to completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 set fish_greeting
 source ~/.config/fish/conf.d/done.fish
 source ~/.config/fish/functions/fish_prompt.fish
+source ~/.config/fish/completions/bazel.fish
 if test -f ~/.config/fish/(hostname)
    source ~/.config/fish/(hostname)
 end
